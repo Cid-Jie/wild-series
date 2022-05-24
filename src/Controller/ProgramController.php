74da77c9f21356fcbe8db60eec1e68b51c2cb5e6
Cid-Jie <pcidjie@gmail.com>
2022-05-24 10:33:50 +0200

    add entity and migration in the database

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/program/', name: 'program_')]
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
