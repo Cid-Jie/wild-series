4e8766ecbd6b00ea5ed460967455691dde2c8cd7
Cid-Jie <pcidjie@gmail.com>
2022-05-30 13:50:39 +0200

    add slug

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,6 +9,7 @@ use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
 use App\Service\Slugify;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -45,7 +46,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/{id<\d+>}/', methods: ['GET'], name: 'show')]
+    #[Route('/{slug}/', methods: ['GET'], name: 'show')]
     public function show(Program $program): Response
     {
  
@@ -63,8 +64,8 @@ class ProgramController extends AbstractController
         ]);
     }
     
-    #[Route("/{program_id}/season/{season_id}", methods: 'GET', name: 'season_show')]
-    #[Entity('program', options: ['id' => 'program_id'])]
+    #[Route("/{program_slug}/season/{season_id}", methods: 'GET', name: 'season_show')]
+    #[ParamConverter('program', options: ['mapping' => ['program_slug' => 'slug']])]
     #[Entity('season', options: ['id' => 'season_id'])]
     public function showSeason(Program $program, Season $season)
     {
@@ -74,10 +75,10 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route("/{program_id}/season/{season_id}/episode/{episode_id}", name: "episode_show")]
-    #[Entity('program', options: ['id' => 'program_id'])]
+    #[Route("/{program_slug}/season/{season_id}/episode/{episode_slug}", name: "episode_show")]
+    #[ParamConverter('program', options: ['mapping' => ['program_slug' => 'slug']])]
     #[Entity('season', options: ['id' => 'season_id'])]
-    #[Entity('episode', options: ['id' => 'episode_id'])]
+    #[ParamConverter('episode', options: ['mapping' => ['episode_slug' => 'slug']])]
     public function showEpisode(Program $program, Season $season, Episode $episode)
     {
         return $this->render('program/episode_show.html.twig', [
